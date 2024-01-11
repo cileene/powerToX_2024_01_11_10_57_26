73d54a4b9b4ba7f27dee6dae343fd9dc9e2c5efb
cileene <106276740+cileene@users.noreply.github.com>
2024-01-11 12:25:57 +0100

    Add image loading and drawing functionality

--- sketch.js
+++ sketch.js
@@ -1,11 +1,15 @@
+// declare variables
 var gylle;
 var diagram;
 var hiNick = 0;
 
-function preload(){
+// load the image before the program starts
+function preload() {
   gylle = loadImage("gylle.png");
+  lastbil = loadImage("lastbil.png");
 }
 
+// setup the canvas
 function setup() {
   createCanvas(800, 400);
   diagram = new Diagram();
@@ -14,13 +18,29 @@ function setup() {
   console.log("Hi nick");
 }
 
+
+
+// draw to the canvas each frame
 function draw() {
   background(220);
   diagram.draw();
-  image(gylle, 75, 125);   
-  
+  image(gylle, 75, 125);
+
+  // draw the lastbil image to the bottom right of the canvas
+  image(lastbil, 500, 270);
+
   // add 1 to the value of hiNick each time draw is called
   hiNick = hiNick + 1;
   // write Hi Nick! + the value of hiNick to the console
   console.log("Hi Nick!", hiNick);
+
+  // when P is pressed, pump gylle
+  if (keyIsPressed && key == "p") {
+
+    // write "p is pressed" to the console
+    console.log("p is pressed");
+
+    // call the pumpGylle function from the diagram class
+    diagram.pumpGylle();
+  }
 }
