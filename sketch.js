fcc1ed5b5fc7e9dee2ad35ba973ff0c21f9e7ae1
cileene <106276740+cileene@users.noreply.github.com>
2024-01-11 12:15:09 +0100

    Add hiNick variable and console logs

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 var gylle;
 var diagram;
+var hiNick = 0;
 
 function preload(){
   gylle = loadImage("gylle.png");
@@ -8,10 +9,18 @@ function preload(){
 function setup() {
   createCanvas(800, 400);
   diagram = new Diagram();
+
+  // test that the program is running by writing "Hi nick" to the console
+  console.log("Hi nick");
 }
 
 function draw() {
   background(220);
   diagram.draw();
   image(gylle, 75, 125);   
+  
+  // add 1 to the value of hiNick each time draw is called
+  hiNick = hiNick + 1;
+  // write Hi Nick! + the value of hiNick to the console
+  console.log("Hi Nick!", hiNick);
 }
